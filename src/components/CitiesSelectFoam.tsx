7fdd975fb73460ec6a5439532a6664c5359af159
山下隼樹 <junki@yamashitashunjunoMacBook-Pro.local>
2022-11-17 16:27:55 +0900

    #3 都道府県入力フォームのコンポーネント作成

---
+++ src/components/CitiesSelectFoam.tsx
@@ -0,0 +1,75 @@
+import { Dispatch, SetStateAction, useRef, useState } from "react";
+import { availableCities } from "../lib/availableCities";
+import styles from "../styles/Weather.module.css";
+import { CityWeather } from "../lib/WeatherInfoController";
+
+type CitiesSelectFoamIn = {
+  setCityIndex: Dispatch<SetStateAction<number>>;
+  weathers: CityWeather;
+};
+type CitiesSelectFoamOut = JSX.Element;
+type getIndexInFirst = string | undefined;
+type getIndexInSecond = CityWeather;
+type getIndexOut = number;
+
+const citiesList = availableCities();
+export const CitiesSelectFoam = ({
+  setCityIndex,
+  weathers,
+}: CitiesSelectFoamIn): CitiesSelectFoamOut => {
+  const [error, seterror] = useState("");
+  const inputEl = useRef<HTMLInputElement>(null);
+  const handleClick = () => {
+    const value = inputEl.current?.value;
+    const includeList = Object.keys(citiesList).filter(
+      (city) => citiesList[city].name === value
+    );
+    if (includeList.length === 1) {
+      setCityIndex(getIndex(value, weathers));
+      seterror("");
+    } else if (includeList.length !== 1) {
+      seterror("正しく都道府県を入力してください");
+    }
+  };
+
+  return (
+    <div>
+      <input
+        ref={inputEl}
+        id="name"
+        type="text"
+        list="item"
+        placeholder="例：東京都/新宿区"
+        autoComplete="off"
+      />
+      <datalist id="item">
+        {Object.entries(citiesList).map((city) => {
+          return <option key="index" value={city[1].name}></option>;
+        })}
+      </datalist>
+      <button onClick={handleClick} className={styles.submit}>
+        送信
+      </button>
+      <div className={styles["explanation-text"]}>
+        <div>各都道府県&#40;県庁所在地&#41;の天気予報がご覧いただけます。</div>
+        <div>「〇〇県/〇〇市」の形で入力し「送信」ボタンを押してください。</div>
+        <div>都道府県名や都市名を一部入力すると予測変換にて表示されます。</div>
+      </div>
+      <div className={styles["error-text"]}>{error}</div>
+    </div>
+  );
+};
+
+const getIndex = (
+  name: getIndexInFirst,
+  weathers: getIndexInSecond
+): getIndexOut => {
+  let reading: undefined | string;
+  Object.entries(citiesList).forEach((city) => {
+    if (name == city[1].name) {
+      reading = city[0];
+    }
+  });
+  const index = weathers.findIndex(({ city }) => city === reading);
+  return index;
+};
